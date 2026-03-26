e566518532db76da551386660575fb2954be878f
DingYi <107735895+dingyi189@users.noreply.github.com>
2026-03-26 16:33:44 +0800

    Update app_update.lua

--- applications/luci-app-passwall/luasrc/model/cbi/passwall/client/app_update.lua
+++ applications/luci-app-passwall/luasrc/model/cbi/passwall/client/app_update.lua
@@ -5,7 +5,7 @@ m = Map(appname)
 api.set_apply_on_parse(m)
 
 -- [[ App Settings ]]--
-s = m:section(TypedSection, "global_app", translate("App Update"),
+s = m:section(TypedSection, "global_app", translate("App Update"))
 s.anonymous = true
 s:append(Template(appname .. "/app_update/app_version"))
 
