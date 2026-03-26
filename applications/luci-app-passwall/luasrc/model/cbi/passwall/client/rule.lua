d57fb64691a2f56f62c1132807b477b840a097d5
DingYi <107735895+dingyi189@users.noreply.github.com>
2026-03-26 16:39:57 +0800

    Update rule.lua

--- applications/luci-app-passwall/luasrc/model/cbi/passwall/client/rule.lua
+++ applications/luci-app-passwall/luasrc/model/cbi/passwall/client/rule.lua
@@ -150,7 +150,7 @@ end
 s:append(Template(appname .. "/rule/rule_version"))
 
 if has_xray or has_singbox then
-	s = m:section(TypedSection, "shunt_rules", "Sing-Box/Xray " .. translate("Shunt Rule"), "<a style='color: red'>" .. translate("Please note attention to the priority, the higher the order, the higher the priority.") .. "</a>")
+	s = m:section(TypedSection, "shunt_rules", "Sing-Box/Xray " .. translate("Shunt Rule"))
 	s.template = "cbi/tblsection"
 	s.anonymous = false
 	s.addremove = true
