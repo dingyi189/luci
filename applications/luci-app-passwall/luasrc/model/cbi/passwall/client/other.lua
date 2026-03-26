582c2181b6c884e29a7964082f8d56ab801d0255
DingYi <107735895+dingyi189@users.noreply.github.com>
2026-03-26 16:19:45 +0800

    Update other.lua

--- applications/luci-app-passwall/luasrc/model/cbi/passwall/client/other.lua
+++ applications/luci-app-passwall/luasrc/model/cbi/passwall/client/other.lua
@@ -102,13 +102,6 @@ o:value("1:65535", translate("All"))
 o:value("53", "DNS")
 o.validate = port_validate
 
-o = s:option(DummyValue, "tips", "　")
-o.rawhtml = true
-o.cfgvalue = function(t, n)
-	return string.format('<font color="red">%s</font>',
-	translate("The port settings support single ports and ranges.<br>Separate multiple ports with commas (,).<br>Example: 21,80,443,1000:2000."))
-end
-
 ---- Use nftables
 o = s:option(ListValue, "prefer_nft", translate("Prefer firewall tools"))
 o.default = "1"
